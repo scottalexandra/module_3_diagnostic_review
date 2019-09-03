b7c9b1e0aeb8c0b99ec5bf7e216cec1618880c37
Alex Robinson <alexandra.scott335@gmail.com>
2019-09-03 08:49:40 -0600

    step 5: split controller code into smaller methods

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,22 +7,44 @@ class SearchController < ApplicationController
   end
 
   def stations(zip)
-    # use faraday to make api call to nrel api limit 10, radius 6 miles, only electric and propane
-    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6&limit=10&fuel_type=ELEC, LPG
-    response = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?') do |req|
+    # response = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?') do |req|
+    #   req.params[:location] = zip
+    #   req.params[:radius] = 6
+    #   req.params[:fuel_type] = "ELEC, LPG"
+    #   req.params[:limit] = 10
+    #   req.headers["x-api-key"] = ENV['nrel_api_key']
+    # end
+    response = fetch_data(zip)
+
+    # parsed_response = JSON.parse(response.body, symbolize_names: true)
+    parsed_response = parse_json(response)
+
+    stations_data = parsed_response[:fuel_stations]
+
+    # stations_data.map do |station|
+    #   Station.new(station)
+    # end
+    create_stations(stations_data)
+  end
+
+  def fetch_data(zip)
+    Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?') do |req|
       req.params[:location] = zip
       req.params[:radius] = 6
       req.params[:fuel_type] = "ELEC, LPG"
       req.params[:limit] = 10
       req.headers["x-api-key"] = ENV['nrel_api_key']
     end
-    # parse json response
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
-    # grab stations from response
-    stations_data = parsed_response[:fuel_stations]
-    # map through stations to create station objects
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def create_stations(stations_data)
     stations_data.map do |station|
       Station.new(station)
     end
   end
+
 end
