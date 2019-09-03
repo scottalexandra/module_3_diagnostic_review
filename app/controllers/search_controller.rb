9d4978339a8919124355201b996fce7b60c9970d
Alex Robinson <alexandra.scott335@gmail.com>
2019-09-03 09:36:31 -0600

    step 8: refactor controller to use a class method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,7 @@
 class SearchController < ApplicationController
   def index
     render locals: {
-      stations: SearchFacade.new(params[:q]).stations
+      stations: SearchFacade.stations(params[:q])
     }
-
   end
-
-  # def stations(zip)
-  #   parsed_response = nrel_service.nearest_stations_by_zip(zip)
-  #   stations_data = parsed_response[:fuel_stations]
-  #   create_stations(stations_data)
-  # end
-  #
-  # def create_stations(stations_data)
-  #   stations_data.map do |station|
-  #     Station.new(station)
-  #   end
-  # end
-  #
-  # def nrel_service
-  #   NrelService.new
-  # end
 end
--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -5,19 +5,23 @@ class SearchFacade
     @zip = zip
   end
 
+  def self.stations(zip)
+    new(zip).stations
+  end
+
   def stations
     parsed_response = nrel_service.nearest_stations_by_zip(zip)
     stations_data = parsed_response[:fuel_stations]
     create_stations(stations_data)
   end
 
+  private
+
   def create_stations(stations_data)
     stations_data.map do |station|
       Station.new(station)
     end
   end
-
-  private
 
   def nrel_service
     NrelService.new
